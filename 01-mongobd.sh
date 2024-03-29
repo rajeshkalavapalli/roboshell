656ee2eb84a3222445ecd025c7eec5650f7cd589
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-29 08:11:46 +0530

    mongodb creation file corrected

--- 01-mongobd.sh
+++ 01-mongobd.sh
@@ -2,43 +2,53 @@
 
 ID=$(id -u)
 
+TIMESTAMP=$(date +%F-%m-%S)
+
+LOGFILE=/tmp/$0-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
 
 validation(){
 
     if [ $1 -ne 0 ]
 then 
-    echo -e "$2 ...........failed"
+    echo -e "$R $2 ...........failed $N"
 else
-    echo -e "$2............success"
+    echo -e "$G $2............success $N"
 
 fi
 }
 
 if [ $ID -ne 0 ]
 then 
-    echo -e "please run this script with root user"
+    echo -e " $R please run this script with root user $N"
     exit 1
 else
-    echo -e "you're a root user"
+    echo -e "$G you're a root user $N"
 
-    cp /home/centos/roboshell/mongo.repo /etc/yum.repos.d/mongo.repo
+    cp /home/centos/roboshell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
     validation $? "coyping repo"
 
 
-    dnf install mongodb-org -y 
+    dnf install mongodb-org -y  &>> $LOGFILE
     validation $? "installing mongo shell"
 
-    systemctl enable mongod
-    validation $? "enabling mangod"
+    systemctl enable mongod &>> $LOGFILE
+    validation $? "enabling mongod"
 
-    systemctl start mongod
-    validation $? " starting  mangod"
+    systemctl start mongod &>> $LOGFILE
+    validation $? " starting  mongod"
 
-    sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf 
+    sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
     validation $? " remote acces"
 
-    systemctl restart mongod
-    validation $? " restarting  momgod"
+    systemctl restart mongod &>> $LOGFILE
+    validation $? " restarting  mongod"
 
 
 fi
